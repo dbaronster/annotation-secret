9b9367c63a615b94f7230f161adec387cd6e7142
Doug Baron <dbaronster@gmail.com>
2025-07-10 09:20:10 -0500

    cleanup, print solution to copnsole

--- data-annotation-solution.py
+++ data-annotation-solution.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from pip._vendor.urllib3.packages.six import print_
 
 """
 Recursively extracts the text from a Google Doc.
@@ -32,8 +33,9 @@ from googleapiclient.errors import HttpError
 # Set doc ID, as found at `https://docs.google.com/document/d/YOUR_DOC_ID/edit`
 #DOCUMENT_ID = "1Lvw3M9tucSJrIorzx9YuSeYg79wk4e2FxaX9-yHKxFE"
 DOCUMENT_ID_F = "1ppvMBTBedsQOzplR_8WN4CQKgJv6wJSTFpAHenHvCqY"
-DOCUMENT_ID_X = '1wISxQ_i3J-VXFO8PLxjCTeJIvh6QkL9Dtfh-oFvRJ0I'
-DOCUMENT_ID = DOCUMENT_ID_F
+DOCUMENT_ID_LOCAL = '1wISxQ_i3J-VXFO8PLxjCTeJIvh6QkL9Dtfh-oFvRJ0I'
+DOCUMENT_ID_FAIL = '2PACX-1vTER-wL5E8YC9pxDx43gk8eIds59GtUUk4nJo_ZWagbnrH0NFvMXIw6VWFLpf5tWTZIT9P9oLIoFJ6A'
+DOCUMENT_ID = DOCUMENT_ID_LOCAL
 
 # Set the scopes and discovery info
 SCOPES = "https://www.googleapis.com/auth/documents"
@@ -72,7 +74,7 @@ def dict_to_html_table(num_rows, num_cols, dictionary):
         for col in range(num_cols):
             html_table += '<td>'
             try:
-                html_table += dictionary[str(row)][str(col)]
+                html_table += dictionary[str(num_rows - row - 1)][str(col)]
             except:
                 html_table += '&nbsp;'
             html_table += '</td>'
@@ -80,6 +82,19 @@ def dict_to_html_table(num_rows, num_cols, dictionary):
     html_table += '</tbody></table>'
     html_table += '</html>'
     return html_table
+
+
+def print_dict_to_console(num_rows, num_cols, dictionary):
+    
+    for row in range(num_rows):
+        line_text = ''
+        for col in range(num_cols):
+            try:
+                line_text += dictionary[str(num_rows - row - 1)][str(col)]
+            except:
+                line_text += ' '
+        print(line_text)
+
 
 def read_paragraph_element(element):
     """Returns the text in the given ParagraphElement.
@@ -91,6 +106,45 @@ def read_paragraph_element(element):
     if not text_run:
         return ''
     return text_run.get('content')
+
+
+def decode_table_secret(table):
+    """Traverses a table structured as described in the 
+    DataAnnotation Coding Exercise: Decoding a Secret Message
+
+    
+    Args:
+    table: a document element of type 'table'.
+    """
+
+    tabledict = {}
+    x_max = 0
+    y_max = 0
+    skipped_first = False
+    for row in table.get('tableRows'):
+        if (not skipped_first):
+            skipped_first = True
+        else:
+            cells = row.get('tableCells')
+            """Grab the 0-2 cells from the row"""
+            x_coordinate = read_structural_elements(cells[0].get('content'))
+            cell_unicode = read_structural_elements(cells[1].get('content'))
+            y_coordinate = read_structural_elements(cells[2].get('content'))
+            if (int(x_coordinate) > x_max):
+                x_max = int(x_coordinate)
+            if (int(y_coordinate) > y_max):
+                y_max = int(y_coordinate)
+            #print(f"{x_coordinate} {cell_unicode} {y_coordinate} ")
+            coldict = {x_coordinate:cell_unicode}
+            if (y_coordinate in tabledict):
+                rowdict = tabledict.get(y_coordinate)
+                rowdict.update(coldict)
+            else:
+                tabledict[y_coordinate] = coldict
+    #print(f"dimensions: {y_max}x{x_max} dist: {tabledict}")
+    #html_table = dict_to_html_table(y_max+1, x_max+1, tabledict)
+    #print(html_table)
+    print_dict_to_console(y_max+1, x_max+1, tabledict)
 
 
 def read_structural_elements(elements):
@@ -107,36 +161,8 @@ def read_structural_elements(elements):
             for elem in elements:
                 text += read_paragraph_element(elem).removesuffix('\n')
         elif 'table' in value:
-            # The text in table cells are in nested Structural Elements and tables may
-            # be nested.
             table = value.get('table')
-            skipped_first = False
-            tabledict = {}
-            x_max = 0
-            y_max = 0
-            for row in table.get('tableRows'):
-                if (not skipped_first):
-                    skipped_first = True
-                else:
-                    cells = row.get('tableCells')
-                    """Grab the 0-2 cells from the row"""
-                    x_coordinate = read_structural_elements(cells[0].get('content'))
-                    cell_unicode = read_structural_elements(cells[1].get('content'))
-                    y_coordinate = read_structural_elements(cells[2].get('content'))
-                    if (int(x_coordinate) > x_max):
-                        x_max = int(x_coordinate)
-                    if (int(y_coordinate) > y_max):
-                        y_max = int(y_coordinate)
-                    print(f"{x_coordinate} {cell_unicode} {y_coordinate} ")
-                    coldict = {x_coordinate:cell_unicode}
-                    if (y_coordinate in tabledict):
-                        rowdict = tabledict.get(y_coordinate)
-                        rowdict.update({x_coordinate:cell_unicode})
-                    else:
-                        tabledict[y_coordinate] = {x_coordinate:cell_unicode}
-            print(f"dimensions: {y_max}x{x_max} dist: {tabledict}")
-            html_table = dict_to_html_table(y_max, x_max, tabledict)
-            print(html_table)
+            decode_table_secret(table)
 
         elif 'tableOfContents' in value:
             # The text in the TOC is also in a Structural Element.
